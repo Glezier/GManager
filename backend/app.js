a24dbafd30b8e401e773fb917ee090e2714832aa
Glezier Montalvane <montalvaneglezier@gmail.com>
2026-04-28 14:55:49 -0300

    chore: configue variables required

--- backend/app.js
+++ backend/app.js
@@ -4,12 +4,27 @@ const corsOptions = require('./src/config/corsOptions')
 const helmet = require('helmet') // Comportamento mais seguro no navegador, cuida dos headers
 
 //Validação de variáves de ambiente
-const requiredEnv = ['DB_URL', 'JWT_SECRET']
-const errEnv = requiredEnv.filter((envVar) => !process.env[envVar])
-if (errEnv.length > 0) {
-  throw new Error(
-    `Variáveis de ambiente obrigatórias ausentes: ${errEnv.join(', ')}`
-  )
+if (process.env.NODE_ENV === 'production'){
+  const requiredEnv = [
+    'DB_URL', 'JWT_SECRET', 
+    'REFRESH_TOKEN_SECRET', 
+    'FRONTEND_URL', 
+    'RESEND_API_KEY', 'EMAIL_FROM'
+  ]
+
+  const missingEnv = requiredEnv.filter((envVar) => !process.env[envVar])
+
+  if (missingEnv.length > 0) {
+    throw new Error(
+      `Variaveis de ambiente obrigatorias em producao ausentes: ${missingEnv.join(', ')}`
+    )
+  }
+
+  if (process.env.COOKIE_SECURE !== 'true') {
+    throw new Error('COOKIE_SECURE deve ser true em producao')
+  }
+
+
 }
 
 const express = require('express')
@@ -25,7 +40,11 @@ app.use(cookieParser())
 app.use(cors(corsOptions))
 app.use(helmet())
 app.use(express.json())
-app.set('trust proxy', 1)
+
+if (process.env.NODE_ENV === 'production') {
+  app.set('trust proxy', 1)
+}
+
 
 app.get('/', (req, res) => {
   res.json({ message: "API funcionando" })
